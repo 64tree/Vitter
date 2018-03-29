bccbc83cb10acc0259ba64fed4f77c75a3d9a805
Sam <Sam@DESKTOP--5VDI64>
2018-03-29 12:06:34 +0100

    reads a single char from text file

--- Vitter/Vitter.cpp
+++ Vitter/Vitter.cpp
@@ -8,41 +8,30 @@
 #include <vector>
 #include "Compression.h"
 
+using namespace std;
+
 int main()
 {
+	vector<char> characterlist;
 	string filename;
 	cout << "What's your file name? ";
-	getline(cin, filename);
+	//getline(cin, filename);
 	cout << "Hello " << filename << ".\n";
-	
-	//first i need to read in a file 
-	string Character;
-	int *p_loadprogram;
-	vector<string> list;
+	filename = "Harry.txt";
 
-	int loadprogram[100] = { 0 };
-	p_loadprogram = loadprogram;
+	//open the file 
+	ifstream fin(filename);
 
-	ifstream CompressFile; // To read the file named CompressFile.
-	CompressFile.open(filename.c_str(), ifstream::in); // To open the file.
-	int size = 0; // To clean the array when it opens the file again. 
-
-	if (CompressFile) { // This displays the contents of the file, if it was able to open.    		
-		while (!CompressFile.eof()) { // It keeps reading the file until it ends.
-			list.push_back(Character);
-			CompressFile >> Character;
-			cout << Character << "\n";
-			size++;
-		}
-		cout << "\nThe Character in the file " << filename << " have been read: " << endl;
-
+	char ch;
+	//until the end of the file get the character and place in the vector 
+	while (!fin.eof()) {
+		fin.get(ch);
+		cout << ch  <<endl;
+		characterlist.push_back(ch);
 	}
-	else if (CompressFile.fail()) { // This indicates that the file was not able to open.
-		cout << "\nThe file does not exist. " << endl;
-	}
+	fin.close();
 
-	CompressFile.close(); // To close the file.
-	getchar();
+
 	getchar();
 	getchar();
 
